835a0a26ebfe6edd5f15bc2f69ac5d2e95406652
Martin Kolomazník <kolomaznik.martin@outlook.com>
2021-11-10 20:03:24 +0100

    Initial Commit

---
+++ VirtualClass/VirtualClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualClassApp.swift
+//  VirtualClass
+//
+//  Created by Martin on 10.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct VirtualClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
